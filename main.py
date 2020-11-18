b1c32f9d2eb3eb84adce8bb95f424d32c095b42b
VadyChel <zyzel19@gmail.com>
2020-11-18 16:58:18 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from tkinter import *
+  
+window = Tk()
+window.title("VideoDownloader")
+window.iconbitmap(os.path.dirname(os.path.abspath(__file__))+"/resources/img/logo.ico")
+window.geometry("1200x700")
+
+
+window.mainloop()
