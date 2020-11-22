dc01efe976c338884f1b99987c0f783f280e90b3
VadyChel <zyzel19@gmail.com>
2020-11-22 19:56:50 +0200

    Add func downloading video and audio

--- main.py
+++ main.py
@@ -12,15 +12,24 @@ class VideoDownloader:
 		self.window = window
 		self.progress = 0
 		self.current_stream = None
-		self.thread = None
+		self.current_video = None
+		self.current_audio = None
+		self.video_thread = None
+		self.audio_thread = None
 		self.save_directory = None
 
+		self.create_gui()
+
+	def create_gui(self):
 		self.window.title("VideoDownloader")
 		self.window.iconbitmap(os.path.dirname(os.path.abspath(__file__))+"/resources/img/logo.ico")
 		self.window.geometry("750x350")
 		self.window.resizable(width=False, height=False)
 
-		self.create_gui()
+		self.url_sender = Entry(self.window, width=90)
+		self.url_sender.grid(column=0, row=0, columnspan=3)
+		self.search_btn = Button(self.window, text="Поиск", command=self.clicked)  
+		self.search_btn.grid(column=4, row=0)
 
 	def show_progress_bar(self, stream:Stream, chunk:int, bytes_remaining:int) -> int:
 		new_progress = round(100-((bytes_remaining/stream.filesize_approx)*100))
@@ -29,8 +38,9 @@ class VideoDownloader:
 			self.progress_text.configure(text=str(self.progress)+"%")
 			self.progress_bar["value"] = self.progress
 
-		if self.progress >= 100:
-			self.thread.join()
+		if self.progress >= 100 and self.current_stream is not None:
+			self.current_stream = None
+			self.create_audio_thread()
 
 	def clicked(self):
 		self.yt = YouTube(self.url_sender.get())
@@ -42,18 +52,17 @@ class VideoDownloader:
 		resolutions = sorted(list(set([
 				stream.resolution 
 				for stream in self.yt.streams.filter(
+					custom_filter_functions=[lambda s: (s.subtype == 'mp4') or (s.subtype == 'webm')],
 					type="video",
-					subtype="mp4",
-					progressive=True)])), 
+					progressive=False)])), 
 				key=lambda key: int(key[:-1]),
 				reverse=True
 			)
 		self.combo["values"] = resolutions
 		self.combo.current(0)
 		self.combo.grid(column=2, row=1, padx=25, sticky=S)
-
-		self.download_btn = Button(self.window, text="Скачать!", command=self.download)  
-		self.download_btn.grid(column=4, row=0)
+		self.combo.bind("<<ComboboxSelected>>", self.set_video_resolution)
+		self.video_resolution = resolutions[0]
 
 		self.progress_bar = Progressbar(
 			self.window, 
@@ -65,26 +74,34 @@ class VideoDownloader:
 		self.progress_text = Label(self.window, text=str(self.progress)+"%")
 		self.progress_text.grid(column=1, row=1, sticky=W)
 
-		stream = self.yt.streams.filter(
-			res=self.combo.get(), 
-			type="video",
-			subtype="mp4",
-			progressive=True).first()
-		self.current_stream = stream
+		self.download_btn = Button(self.window, text="Скачать!", command=self.download)  
+		self.download_btn.grid(column=4, row=0)
 
-	def create_gui(self):
-		self.url_sender = Entry(self.window, width=90)
-		self.url_sender.grid(column=0, row=0, columnspan=3)
-		self.search_btn = Button(self.window, text="Поиск", command=self.clicked)  
-		self.search_btn.grid(column=4, row=0)
+	def set_video_resolution(self, event):
+		self.video_resolution = self.combo.get()
+
+	def get_video_stream(self):
+		self.current_video = self.yt.streams.filter(
+			custom_filter_functions=[lambda s: (s.subtype == 'mp4') or (s.subtype == 'webm')],
+			res=self.video_resolution, 
+			type="video",
+			progressive=False).first()
+
+	def get_audio_stream(self):
+		self.current_audio = self.yt.streams.filter(
+			type="audio",
+			subtype="mp4").first()
 
 	def download(self):
 		self.set_save_directory()
 		self.download_btn.destroy()
 		self.combo.destroy()
 
-		self.video_resolution = Label(self.window, text=self.current_stream.resolution)
-		self.video_resolution.grid(column=2, row=1, padx=25, sticky=S)
+		self.get_audio_stream()
+		self.get_video_stream()
+
+		self.stream_type = Label(self.window, text=self.current_video.resolution)
+		self.stream_type.grid(column=2, row=1, padx=25, sticky=S)
 
 		self.stop_btn = Button(self.window, text="Остановить", command=self.kill_thread)  
 		self.stop_btn.grid(column=4, row=0)
@@ -102,14 +119,26 @@ class VideoDownloader:
 		# label = Label(image=photo)
 		# label.pack()
 
-		self.thread = Thread(target=self.download_stream)
-		self.thread.start()
+		self.create_video_thread()
 
 	def set_save_directory(self):
 		self.save_directory = askdirectory()
 		
-	def download_stream(self) -> None:
-		self.current_stream.download(filename=self.current_stream.title, output_path=self.save_directory)
+	def download_video(self) -> None:
+		self.current_video.download(filename="Video", output_path=self.save_directory)
+
+	def download_audio(self) -> None:
+		self.current_audio.download(filename="Audio", output_path=self.save_directory)
+
+	def create_video_thread(self) -> None:
+		self.current_stream = self.current_video
+		self.thread = Thread(target=self.download_video)
+		self.thread.start()
+
+	def create_audio_thread(self) -> None:
+		self.current_stream = self.current_audio
+		self.thread = Thread(target=self.download_audio)
+		self.thread.start()
 
 	def kill_thread(self):
 		self.thread.terminate()
@@ -117,6 +146,6 @@ class VideoDownloader:
 if __name__ == "__main__":
 	root = Tk()
 	downloader = VideoDownloader(root)
-	if downloader.thread is not None:
+	if downloader.video_thread is not None:
 		root.protocol("WM_DELETE_WINDOW", downloader.kill_thread)
 	root.mainloop()
